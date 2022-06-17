7c7b2de7eb176cf4b8740ca2fd896fca494091f4
Urus4 <102502766+Urus4@users.noreply.github.com>
2022-06-17 16:09:18 +0900

    MFC Exam

---
+++ Exam/PicturePuzzleKhm/PicturePuzzleKhmDlg.h
@@ -0,0 +1,50 @@
+﻿
+// PicturePuzzleKhmDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "imageSrc\MyImage.h"
+
+// CPicturePuzzleKhmDlg 대화 상자
+class CPicturePuzzleKhmDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CPicturePuzzleKhmDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PICTUREPUZZLEKHM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+protected:
+	CByteImage m_imageIn1;
+	CByteImage m_imageIn2;
+	CByteImage m_imageOut1;
+	CByteImage m_imageOut2;
+	CByteImage m_imageOutLast;
+
+
+
+	void _ImageOpImage();
+public:
+	afx_msg void OnBnClickedButtonModify();
+	afx_msg void OnBnClickedButtonResult();
+	afx_msg void OnBnClickedButtonPlane();
+};
